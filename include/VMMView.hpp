db9b2a4debd051f0d26107765193341e872b9c45
purpleKarrot <daniel@pfeifer-mail.de>
2010-01-08 13:24:32 +0000

    include all header and cmake file
    git-svn-id: https://coview.svn.sourceforge.net/svnroot/coview/Framework@125 5ee48f18-ff1d-4a81-bb19-9249dcc9e3de

---
+++ include/VMMView.hpp
@@ -0,0 +1,19 @@
+/*
+ * VMMView.hpp
+ *
+ *  Created on: 08.01.2010
+ *      Author: daniel
+ */
+
+#ifndef VMMVIEW_HPP
+#define VMMVIEW_HPP
+
+#include <VMMView/Color.hpp>
+//#include <VMMView/Enum.hpp>
+#include <VMMView/MeshLoader.hpp>
+#include <VMMView/RenderAlgorithm.hpp>
+#include <VMMView/ScopedLocks.hpp>
+#include <VMMView/ShaderProgram.hpp>
+#include <VMMView/Texture.hpp>
+
+#endif /* VMMVIEW_HPP */
