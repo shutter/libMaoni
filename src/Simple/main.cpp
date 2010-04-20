f8ef717a2b50d25e7fa19c05e227bf1ab11c47e8
Daniel Pfeifer <daniel@pfeifer-mail.de>
2010-04-20 10:17:34 +0200

    splashscreen is loaded from resources

--- src/Simple/main.cpp
+++ src/Simple/main.cpp
@@ -19,19 +19,19 @@ int maoni_main(int argc, char* argv[],
 		MeshLoader* mesh_loader_stack)
 {
 	QApplication app(argc, argv);
+	Q_INIT_RESOURCE(Resources);
 
-	QSplashScreen splash(QPixmap("libmaoni_sscr.png"), Qt::WindowStaysOnTopHint);
+	QPixmap pixmap(":/Maoni/Splashscreen.png");
+	QSplashScreen splash(pixmap, Qt::WindowStaysOnTopHint);
     splash.show();
     splash.showMessage("Loading Widgets...");
+	QTimer::singleShot(3000, &splash, SLOT(close()));
 
 	FrameData framedata(algorithm_factory_stack, mesh_loader_stack);
 
 	MainWindow main_window(framedata, new RenderWidget(framedata));
 	main_window.setWindowTitle("Maoni");
 	main_window.show();
-
-	QTimer::singleShot(3000, &splash, SLOT(close()));
-    //splash.finish(&main_window);
 
 	return app.exec();
 }
