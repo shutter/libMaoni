ecf76e0de28b4bc483d94e6d9f98b91ae2c25752
Stefan Hutter <shutter@icu.uzh.ch>
2010-05-04 12:48:22 +0200

    modified build from scratch tutorial and color.hpp commented (test)

--- include/Maoni/Color.hpp
+++ include/Maoni/Color.hpp
@@ -1,20 +1,44 @@
 /*
- * Color.hpp
+ * libMaoni common viewing framework
+ * Copyright (C) 2009, 2010 Daniel Pfeifer
  *
- *  Created on: 08.12.2009
- *      Author: daniel
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef VMMVIEW_COLOR_HPP
 #define VMMVIEW_COLOR_HPP
 
+//! A color class
+/*!
+  The color class is used to exchange color information from GUI or loader
+  to algorithms to the OpenGL state machine. It consists of arithmetic (0.f to 1.f) RGBA values
+*/
+
+
 class Color
 {
 public:
+    //! Creating a color object from scratch initialized by 1.0f
 	Color()
 	{
 		impl[0] = impl[1] = impl[2] = impl[3] = 1.f;
 	}
+
+    //! Creating a color object from another color object reference
+    /*!
+      \param other a color reference argument.
+    */
 
 	Color(Color const& other)
 	{
@@ -24,6 +48,13 @@ public:
 		impl[3] = other.alpha();
 	}
 
+    //! Creating a color object from RGBA values
+    /*!
+      \param red a float argument.
+      \param green a float argument.
+      \param blue a float argument.
+      \param alpha a float argument.
+    */
 	Color(float red, float green, float blue, float alpha)
 	{
 		impl[0] = red;
@@ -32,26 +63,46 @@ public:
 		impl[3] = alpha;
 	}
 
+    //! The red value getter
+    /*!
+      \return The arithmetic red color value
+    */
 	float red() const
 	{
 		return impl[0];
 	}
 
+    //! The green value getter
+    /*!
+      \return The arithmetic green color value
+    */
 	float green() const
 	{
 		return impl[1];
 	}
 
+    //! The blue value getter
+    /*!
+      \return The arithmetic blue color value
+    */
 	float blue() const
 	{
 		return impl[2];
 	}
 
+    //! The alpha value getter
+    /*!
+      \return The arithmetic alpha opacity value
+    */
 	float alpha() const
 	{
 		return impl[3];
 	}
 
+	//! The RGBA array getter
+    /*!
+      \return The arithmetic RGBA float array pointer
+    */
 	operator const float*() const
 	{
 		return impl;
