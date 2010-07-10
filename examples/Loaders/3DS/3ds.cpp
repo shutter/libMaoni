b551d4806c7014dfdb62ab0cd8acd335fd45d352
Stefan Hutter <shutter@icu.uzh.ch>
2010-07-10 16:52:40 +0200

    fixed 3ds loader

--- examples/Loaders/3DS/3ds.cpp
+++ examples/Loaders/3DS/3ds.cpp
@@ -9,6 +9,7 @@
 #include <lib3ds/file.h>
 #include <lib3ds/mesh.h>
 #include <stdexcept>
+#include <iostream>
 
 MESH_LOADER(3ds, 3D Studio Max Model)
 {
@@ -18,26 +19,28 @@ MESH_LOADER(3ds, 3D Studio Max Model)
 	if (!file)
 		throw std::runtime_error("Loading file failed");
 
-	int offset = 0;
-	for (Lib3dsMesh* m = file->meshes; m; m = m->next)
-	{
-		// copy vertices
-		for (Lib3dsDword i = 0; i < m->points; i++)
+	unsigned int offset = 0;
+
+		for (Lib3dsMesh* mesh = file->meshes; mesh != NULL; mesh = mesh->next)
 		{
-			Lib3dsVector& pos = m->pointL[i].pos;
+			// copy vertices
+			for (Lib3dsDword i = 0; i < mesh->points; i++)
+			{
+				Lib3dsVector& pos = mesh->pointL[i].pos;
 
-			model.add_vertex(Vertex(pos[0], pos[1], pos[2]));
+				model.add_vertex(Vertex(pos[0], pos[1], pos[2]));
 
-			++offset;
+			}
+
+			// copy nodes to triangles
+			for (Lib3dsDword i = 0; i < mesh->faces; i++)
+			{
+				Lib3dsWord* points = mesh->faceL[i].points;
+				model.add_triangle(points[0] + offset, points[1] + offset, points[2] + offset);
+			}
+
+			offset += mesh->points;
 		}
-
-		// copy nodes to triangles
-		for (Lib3dsDword i = 0; i < m->faces; i++)
-		{
-			Lib3dsWord* points = m->faceL[i].points;
-			model.add_triangle(points[0] + offset, points[1] + offset, points[2] + offset);
-		}
-	}
 
 	model.calculate_normals();
 	model.fix_scale();
