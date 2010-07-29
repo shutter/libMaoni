e91f75cabaf02ca48fbf6bdc3bdb76600e58d2b1
shutter <shutter@icu.uzh.ch>
2010-07-29 15:03:41 +0200

    added texture mapping

--- examples/Algorithms/SphereMapping.cpp
+++ examples/Algorithms/SphereMapping.cpp
@@ -4,43 +4,47 @@
 
 #include <GL/glew.h>
 #include <Maoni.hpp>
+#include "../Loaders/VBOModel.hpp"
+
 
 SHADER_SOURCE(vertex_source, (version 120),
 
-	varying vec3 normal;
+		varying vec3 normal;
 
-	void main()
-	{
-		normal = gl_NormalMatrix * gl_Normal;
+		void main()
+		{
+			normal = gl_NormalMatrix * gl_Normal;
 
-		gl_Position = ftransform();
-	}
+			gl_Position = ftransform();
+		}
 );
 
 SHADER_SOURCE(fragment_source, (version 120),
 
-	uniform sampler2D texture;
-	varying vec3 normal;
+		uniform sampler2D texture;
+		varying vec3 normal;
 
-	void main()
-	{
-		gl_FragColor = texture2D(texture, vec2(0.5) + 0.5 * normalize(normal).xy);
-	}
+		void main()
+		{
+			gl_FragColor = texture2D(texture, vec2(0.5) + 0.5 * normalize(normal).xy);
+		}
 );
 
 SHADER_PROGRAM(SphereMappingShader,
-	(VERTEX, vertex_source)(FRAGMENT, fragment_source),
+		(VERTEX, vertex_source)(FRAGMENT, fragment_source),
 );
 
 RENDER_ALGORITHM(SphereMapping,
-	(ShaderProgram, shader, SphereMappingShader())
-	(Texture, sphere_texture, "../examples/Models/sphere3.jpg")
+		(ShaderProgram, shader, SphereMappingShader())
+		(Texture, sphere_texture, "../examples/Models/sphere3.jpg")
 )
 {
 	ScopedDisable lighting_lock(GL_LIGHTING);
 	ScopedEnable texture_2D_lock(GL_TEXTURE_2D);
-	ScopedBindTexture texture_lock(sphere_texture);
+	ScopedBindTexture texture_lock(sphere_texture, GL_TEXTURE0);
 	ScopedUseProgram shader_lock(shader);
+
+	glUniform1i(glGetUniformLocation(shader, "texture"), 0);
 
 	model.draw();
 }
