f1e3aa69840b124756802dbecb27985d172c9a95
Daniel Pfeifer <daniel@pfeifer-mail.de>
2010-07-31 17:17:42 +0200

    some fixes

--- CMake/FindGLEW.cmake
+++ CMake/FindGLEW.cmake
@@ -2,46 +2,35 @@
 # Once done this will define
 #  
 #  GLEW_FOUND        - system has GLEW
-#  GLEW_INCLUDE_DIR  - the GLEW include directory
-#  GLEW_LIBRARY_DIR  - where the libraries are
-#  GLEW_LIBRARY      - Link these to use GLEW
+#  GLEW_INCLUDE_DIRS - the GLEW include directory
+#  GLEW_LIBRARIES    - Link these to use GLEW
 #   
 
-IF (GLEW_INCLUDE_DIR)
-  # Already in cache, be silent
-  SET(GLEW_FIND_QUIETLY TRUE)
-ENDIF (GLEW_INCLUDE_DIR)
+find_path(GLEW_INCLUDE_DIRS glew.h
+  PATHS
+    /usr/include
+    /usr/local/include
+    /opt/local/include
+    c:/glew/include
+  PATH_SUFFIXES
+    gl/
+    GL/
+  )
 
-if( WIN32 )
-   if( MSVC80 )
-       set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ 8/VC" )
-   endif( MSVC80 )
-   if( MSVC71 )
-       set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ .NET\ 2003/Vc7" )
-   endif( MSVC71 )
-   FIND_PATH( GLEW_INCLUDE_DIR gl/glew.h gl/wglew.h
-              PATHS c:/glew/include ${COMPILER_PATH}/PlatformSDK/Include )
-   SET( GLEW_NAMES glew32 )
-   FIND_LIBRARY( GLEW_LIBRARY
-                 NAMES ${GLEW_NAMES}
-                 PATHS c:/glew/lib ${COMPILER_PATH}/PlatformSDK/Lib )
-else( WIN32 )
-   FIND_PATH( GLEW_INCLUDE_DIR glew.h wglew.h
-              PATHS /usr/local/include /usr/include
-              PATH_SUFFIXES gl/ GL/ )
-   SET( GLEW_NAMES glew GLEW )
-   FIND_LIBRARY( GLEW_LIBRARY
-                 NAMES ${GLEW_NAMES}
-                 PATHS /usr/lib /usr/local/lib )
-endif( WIN32 )
+find_library(GLEW_LIBRARIES
+  NAMES
+    glew
+    glew32
+    GLEW
+  PATHS
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    c:/glew/lib
+  )
 
-GET_FILENAME_COMPONENT( GLEW_LIBRARY_DIR ${GLEW_LIBRARY} PATH )
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GLEW
+  DEFAULT_MSG GLEW_LIBRARIES GLEW_INCLUDE_DIRS)
 
-IF (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-   SET(GLEW_FOUND TRUE)
-    SET( GLEW_LIBRARY_DIR ${GLEW_LIBRARY} )
-ELSE (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-   SET( GLEW_FOUND FALSE )
-   SET( GLEW_LIBRARY_DIR )
-ENDIF (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-
+mark_as_advanced(GLEW_INCLUDE_DIRS GLEW_LIBRARIES)
